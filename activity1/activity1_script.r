a3a2eb13e1ad7aa0233efcaf5f4b1e05cb30f934
Matt Gentile '20 <mgentile@colgate.edu>
2020-01-24 12:02:03 -0500

    added R code for calculations in activity1 script

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -1,3 +1,27 @@
 #Activity 1
 
 print("hello R world")
+
+
+# basic calculations
+3 + 4
+5^3
+16-20
+
+
+# set variable with value
+var <- 4
+
+
+# perform calculations with variable
+var - 1
+var^2
+
+
+# create vector variable 
+vec <- c(12,14,100,101)
+
+
+# perform calculations to each element of vector
+vec - 1
+vec*2
