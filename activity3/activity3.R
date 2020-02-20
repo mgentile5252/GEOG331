a34ab53697161ced64daff7593680b494be2a7f0
Matt Gentile '20 <mgentile@colgate.edu>
2020-02-19 19:23:53 -0500

    updated code through question 4

--- activity3/activity3.R
+++ activity3/activity3.R
@@ -52,28 +52,109 @@ sensorInfo <-   read.csv("y:\\Students\\mgentile\\a03\\bewkes_weather.csv",
                          na.strings=c("#N/A"), nrows=2)
 
 print(sensorInfo)
+#names(sensorInfo)
+#names(datW)
+
+
+
+# get column names from sensorInfo table
+# and set weather station colnames  to be the same
+
+colnames(datW) <-   colnames(sensorInfo)
+#preview data
+print(datW[1,])
+
+
+
+
+############## DATA QA/QC ############## 
+#install.packages("lubridate")
+library("lubridate")
+
+
+#convert to standardized format
+#date format is m/d/y
+dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
+
+
+
+#calculate day of year
+datW$doy <- yday(dates)
+#calculate hour in the day
+datW$hour <- hour(dates) + (minute(dates)/60)
+#calculate decimal day of year
+datW$DD <- datW$doy + (datW$hour/24)
+#quick preview of new date calcualtions
+datW[1,]
+
+
+
+# Look into amount of missing data (NA)
+#see how many values have missing data for each sensor observation
+#air temperature
+
+#how many times is.na returns true for the air temperature column
+length(which(is.na(datW$air.temperature)))
+
+length(which(is.na(datW$wind.speed)))
+
+length(which(is.na(datW$precipitation)))
+
+length(which(is.na(datW$soil.moisture)))
+
+length(which(is.na(datW$soil.temp)))
+
+
+
+#make a plot with filled in points (using pch)
+#line lines
+
+#data abruptly stops in the second week of July
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (cm3 water per cm3 soil)")
+
+
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
+
+
+
+#make a new column to work with that indicates that I am conducting QAQC
+#if air temp is below 0, recird NA
+#if air temp is not below 0 record actual air temp
+datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
 
 
 
 
 
 
+#check the values at the extreme range of the data and throughout the percentiles
+quantile(datW$air.tempQ1)
 
 
 
 
+#look at days with really low air temperature
+datW[datW$air.tempQ1 < 8,]  
+
+#look at days with really high air temperature
+datW[datW$air.tempQ1 > 33,]  
 
 
 
+# QUESTION 4
+datW[datW$air.tempQ1 < 8, 1]  
 
+datW[datW$air.tempQ1 < 8, 14]  
+#173 177 177 177 177 177
+# June 22nd and June 26th
 
-
-
-
-
-
-
-
-
-
+datW[datW$air.tempQ1 > 33,1]  
+datW[datW$air.tempQ1 > 33,14]  
+#182 182 182 182 182 182 182 183
+# July 1 amd July 2
 
